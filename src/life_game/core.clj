1b55f8f557459a70b1e5066a1f35011e4b7d7e7d
aiya000 <aiya000.develop@gmail.com>
2016-03-15 22:09:16 +0900

    Improve code core.clj

--- src/life_game/core.clj
+++ src/life_game/core.clj
@@ -5,18 +5,10 @@
 
 (def life-speed 200)
 
-(defn draw-all [cells scr]
-  (if (empty? cells)
-    nil
-    (let [c (first cells) cs (rest cells)]
-      (do
-        (.draw c scr)
-        (recur cs scr)))))
-
 (defn game-core [cells scr]
   (do
     (screen/clear scr)
-    (draw-all cells scr)
+    (doall (map #(.draw % scr) cells))
     (Thread/sleep life-speed)
     (let [[scr-width scr-height] (screen/get-size scr)
           x (int (rand scr-width))
