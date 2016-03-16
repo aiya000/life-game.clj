55d4374e3125c906fd1e9bae40699d6002a31da7
aiya000 <aiya000.develop@gmail.com>
2016-03-16 22:33:12 +0900

    Create nebra to screen
    Remove my universe...

--- src/life_game/core.clj
+++ src/life_game/core.clj
@@ -1,24 +1,22 @@
 (ns life-game.core
   (:gen-class)
-  (:require [lanterna.screen     :as screen]
-            [life-game.data.cell :as cell]))
+  (:require [lanterna.screen       :as screen]
+            [life-game.data.cell   :as cell]
+            [life-game.data.values :as values]))
 
 (def life-speed 200)
 
-(defn game-core [cells scr]
+(defn game-core
+  "life-game's main loop"
+  [cells scr]
   (do
     (screen/clear scr)
     (doall (map #(.draw % scr) cells))
     (Thread/sleep life-speed)
-    (let [[scr-width scr-height] (screen/get-size scr)
-          x (int (rand scr-width))
-          y (int (rand scr-height))
-          c (cell/new-cell x y)]
-      (recur (conj cells c) scr))))
+    (recur cells scr)))
 
 (defn -main []
-  (let [scr (screen/get-screen)
-        cells #{}]
+  (let [scr (screen/get-screen)]
     (screen/start scr)
-    (game-core cells scr)
+    (game-core values/nebra scr)
     (screen/stop scr)))
