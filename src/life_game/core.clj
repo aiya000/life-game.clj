4564b630a3ded80500e2b44e21575c1934d1fabf
aiya000 <aiya000.develop@gmail.com>
2016-03-17 23:49:00 +0900

    Implment birthing cells

--- src/life_game/core.clj
+++ src/life_game/core.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require [lanterna.screen       :as screen]
             [life-game.data.cell   :as cell]
-            [life-game.data.values :as values]))
+            [life-game.data.values :as values]
+            [life-game.life        :as life]))
 
 (def life-speed 200)
 
@@ -12,11 +13,14 @@
   (do
     (screen/clear scr)
     (doall (map #(.draw % scr) cells))
-    (Thread/sleep life-speed)
-    (recur cells scr)))
+    (screen/get-key-blocking scr) ;NOTE: for debug
+    ;(Thread/sleep life-speed)
+    (let [birthed-cells (life/birth cells)]
+      (recur birthed-cells scr))))
 
 (defn -main []
   (let [scr (screen/get-screen)]
     (screen/start scr)
-    (game-core values/nebra scr)
+    (game-core values/basic-birth scr) ;NOTE: for debug
+    ;(game-core values/nebra scr)
     (screen/stop scr)))
--- src/life_game/data/values.clj
+++ src/life_game/data/values.clj
@@ -1,6 +1,10 @@
 (ns life-game.data.values
   (:require [life-game.data.cell :as cell]))
 
+(def basic-birth #{(cell/new-cell 5 5)
+                   (cell/new-cell 6 5)
+                   (cell/new-cell 5 6)})
+
 (def nebra #{(cell/new-cell  5  5)
              (cell/new-cell  6  5)
              (cell/new-cell  7  5)
