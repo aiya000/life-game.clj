f3d77ccca7ab9d17d0569c8b09dddea305fc7bcf
aiya000 <aiya000.develop@gmail.com>
2016-04-03 15:41:12 +0900

    Add test 'die-overcrowded-test'

--- test/life_game/life_test.clj
+++ test/life_game/life_test.clj
@@ -13,12 +13,24 @@
 (def basic-die-depop #{(cell/new-cell 1 1)
                        (cell/new-cell 1 2)})
 
+; THese cells will dead by overcrowded
+(def basic-die-over #{(cell/new-cell 1 1)
+                      (cell/new-cell 1 2)
+                      (cell/new-cell 1 3)
+                      (cell/new-cell 2 1)
+                      (cell/new-cell 2 2)})
+
 (deftest birth-test
   (testing "birth func generate birthed cells rightly."
     (let [birth-cells (life/birth basic-birth)]
       (is (contains? birth-cells (cell/new-cell 2 2))))))
 
-(deftest die-test
-  (testing "die func generate dead cells rightly."
+(deftest die-depopulated-test
+  (testing "die-by-depop func generate dead cells rightly."
     (let [dead-cells (life/die-by-depop basic-die-depop)]
       (is (empty? (cset/difference dead-cells basic-die-depop))))))
+
+(deftest die-overcrowded-test
+  (testing "die-by-over func generate dead cells rightly."
+    (let [dead-cells (life/die-by-over basic-die-over)]
+      (is (empty? (cset/difference dead-cells basic-die-over))))))
