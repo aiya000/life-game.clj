23badfa2ea2c6c84b13b677fbf59c81450ef7f78
aiya000 <aiya000.develop@gmail.com>
2016-04-05 21:31:34 +0900

    (TMP) Remove debug operations
    Fix nebra value

--- src/life_game/core.clj
+++ src/life_game/core.clj
@@ -1,11 +1,20 @@
 (ns life-game.core
   (:gen-class)
   (:require [lanterna.screen       :as screen]
+            [clojure.set           :as cset]
             [life-game.data.cell   :as cell]
             [life-game.data.values :as values]
             [life-game.life        :as life]))
 
 (def life-speed 200)
+
+(defn- remove-out-of-bounds-cells
+  "Remove unused cells"
+  [cells]
+  (do
+    (defn- is-out [c] (or (< (.x c) 0)
+                          (< (.y c) 0)))
+    (set (for [x cells :when (not (is-out x))] x))))
 
 (defn game-core
   "life-game's main loop"
@@ -13,16 +22,17 @@
   (do
     (screen/clear scr)
     (doall (map #(.draw % scr) cells))
-    (screen/get-key-blocking scr) ;NOTE: for debug
-    ;(Thread/sleep life-speed)
-    (let [birthed-cells (life/birth cells)
-          dead-cells-1 (life/die-by-depop cells)
-          dead-cells-2 nil]
-      (recur dead-cells-1 scr))))
+    (Thread/sleep life-speed)
+    (let [birthed-cells (-> cells life/birth        remove-out-of-bounds-cells)
+          dead-cells-1  (-> cells life/die-by-depop remove-out-of-bounds-cells)
+          dead-cells-2  (-> cells life/die-by-over  remove-out-of-bounds-cells)
+          increased-cells (cset/union cells birthed-cells)
+          dead-cells      (cset/union dead-cells-1 dead-cells-2)
+          result-cells    (cset/difference increased-cells dead-cells)]
+      (recur result-cells scr))))
 
 (defn -main []
   (let [scr (screen/get-screen)]
     (screen/start scr)
-    (game-core values/basic-die scr) ;NOTE: for debug
-    ;(game-core values/nebra scr)
+    (game-core values/nebra scr)
     (screen/stop scr)))
